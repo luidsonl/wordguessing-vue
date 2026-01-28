f81210e6dadc3a0b2ba6c38e9c559d990e969727
Luidson Lima Santos <luidsonlsti@gmail.com>
2026-01-28 17:58:57 -0300

    feat: setup implement word fetching

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,18 @@
 import Footer from '@/components/Footer.vue';
 import Header from '@/components/Header.vue';
 import Main from '@/components/Main.vue';
+import { useWordStore } from './stores/WordStore';
+
+import { ref } from 'vue';
+
+const wordStore = useWordStore();
+wordStore.newWordWithLength(5);
 
 
 </script>
 
 <template>
+  <div>{{ wordStore.getWord }}</div>
   <Header/>
   <Main/>
   <Footer/>
---
+++ src/stores/WordStore.js
@@ -0,0 +1,50 @@
+import { defineStore } from 'pinia'
+
+export const useWordStore = defineStore('word', {
+    state() {
+        return {
+            value: '',
+            isLoading: false,
+            error: null
+        }
+    },
+
+    actions: {
+        async fetchWord(url) {
+            this.isLoading = true;
+            this.error = null;
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+
+                if (data.length > 0) {
+                    const randomIndex = Math.floor(Math.random() * data.length);
+                    this.value = data[randomIndex].word;
+                } else {
+                    this.value = '';
+                }
+            } catch (err) {
+                this.error = "Erro ao buscar palavra";
+                console.error(err);
+            } finally {
+                this.isLoading = false;
+            }
+        },
+
+        async newWordWithLength(length) {
+            const pattern = '?'.repeat(length);
+            const url = `https://api.datamuse.com/words?sp=${pattern}`;
+            await this.fetchWord(url);
+        },
+
+        async newWordWithLengthSimilar(length, similar) {
+            const pattern = '?'.repeat(length);
+            const url = `https://api.datamuse.com/words?ml=${similar}&sp=${pattern}`;
+            await this.fetchWord(url);
+        }
+    },
+
+    getters: {
+        getWord: (state) => state.value
+    }
+})
