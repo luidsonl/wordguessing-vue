e71a88313d6d6bca4c6ae74d600dc0baddbdfff5
Luidson Lima Santos <luidsonlsti@gmail.com>
2026-02-10 17:43:01 -0300

    feat: display correct word on game over

--- src/components/GameOver.vue
+++ src/components/GameOver.vue
@@ -8,6 +8,7 @@ const generalStore = useGeneralStore()
     <div class="w-full max-w-3xl mx-auto p-4 border border-gray-300 rounded bg-white mt-4">
         <h2 v-if="generalStore.isWinner" class="text-center text-2xl text-green-800">Congratulations</h2>
         <h2 v-else class="text-center text-2xl text-red-800">You Lost</h2>
+        <div class="text-center mt-3 text-lg">The secret word is <span class="text-2xl text-green-800">{{ generalStore.secretWord.toUpperCase() }}</span></div>
     </div>
 
     <div class="flex justify-center mt-4 text-2xl">
